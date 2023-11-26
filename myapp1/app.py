7c5db2cce117c5523ae0c9904d9e306450670394
cristiscu <cristiscu@gmail.com>
2023-11-26 09:16:58 -0800

    updates

--- myapp1/app.py
+++ myapp1/app.py
@@ -3,8 +3,23 @@ import os
 
 st.write(f"Current folder: {os.path.dirname(__file__)}")
 
-with open("data/file.txt", "r") as file:
-    st.write(f"File.txt content: {file.read()}")
+filename = "data/file.txt"
+try:
+    with open(filename, "r") as file:
+        st.write(f"{filename} content: {file.read()}")
+except:
+    st.error(f"Cannot open {filename}")
 
-with open("../myapp2/data/file.txt", "r") as file:
-    st.write(f"Myapp2 File.txt content: {file.read()}")
+filename = "../myapp2/data/file.txt"
+try:
+    with open(filename, "r") as file:
+        st.write(f"{filename} content: {file.read()}")
+except:
+    st.error(f"Cannot open {filename}")
+
+filename = f"{os.path.dirname(__file__)}/data/file.txt"
+try:
+    with open(filename, "r") as file:
+        st.write(f"{filename} content: {file.read()}")
+except:
+    st.error(f"Cannot open {filename}")
--- myapp2/app.py
+++ myapp2/app.py
@@ -1,10 +1,2 @@
 import streamlit as st
-import os
-
-st.write(f"Current folder: {os.path.dirname(__file__)}")
-
-with open("data/file.txt", "r") as file:
-    st.write(f"File.txt content: {file.read()}")
-
-with open("../myapp1/data/file.txt", "r") as file:
-    st.write(f"Myapp1 File.txt content: {file.read()}")
+st.write("Whatever...")
